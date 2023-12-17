90e8ff2397875581418a5467fdb5d2fb867ec2a9
d-naumov <nd76@gmx.de>
2023-12-17 19:57:04 +0100

    add all for homework_5

---
+++ QA27_First_SeleniumProject/src/test/java/homework_2_5/LoginTests.java
@@ -0,0 +1,29 @@
+package homework_2_5;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class LoginTests extends TestBase {
+
+  @BeforeMethod
+  public void ensurePrecondition() {
+    if (!isLoginLinkPresent()) {
+      clickOnSignOutButton();
+    }
+  }
+
+  @Test
+  public void loginPositiveTest() {
+    //click on the Login link
+    clickOnLoginLink();
+    fillLoginRegisterForm("ssd@gm.com", "Ssdir123$");
+    //click on the Login button
+    clickOnLoginButton();
+    // assert: Log out button is present
+    Assert.assertTrue(isSingOutButtonPresent());
+
+  }
+
+
+}
